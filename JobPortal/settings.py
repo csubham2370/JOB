105c7be15a29e1ff8757eb0275927dfaee3da5dc
csubham2370 <144363196+csubham2370@users.noreply.github.com>
2024-01-12 16:28:23 +0530

    3rd

--- JobPortal/settings.py
+++ JobPortal/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "jobs/static")
+    os.path.join(BASE_DIR, 'staticfiles_build', "jobs/static")
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'jobs/media')
